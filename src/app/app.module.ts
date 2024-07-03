eb3148be7414fc952a9d3fc515b84068e2cceb9f
Leonel <leonel.bruzzese@gmail.com>
2024-07-03 00:46:04 -0300

    FIX centrado web

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { TopInfoComponent } from './components/top-info/top-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    TopInfoComponent
   ],
   imports: [
     BrowserModule,
