5290cbe3e9ec7f937b8b302402920b13e8ded36f
Leonel <leonel.bruzzese@gmail.com>
2024-07-22 10:08:57 -0300

    Se creo el componente areas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ContactComponent } from './components/contact/contact.component';
 import { SendEmailComponent } from './pages/send-email/send-email.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CarouselComponent } from './components/carousel/carousel.component';
+import { AreasComponent } from './components/areas/areas.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { CarouselComponent } from './components/carousel/carousel.component';
     FooterComponent,
     ContactComponent,
     SendEmailComponent,
-    CarouselComponent
+    CarouselComponent,
+    AreasComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/areas/areas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-areas',
+  templateUrl: './areas.component.html',
+  styleUrl: './areas.component.css'
+})
+export class AreasComponent {
+
+}
