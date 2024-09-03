de9b51eccbddc1c754d1b034513787e0f3c2590e
Leonel <leonel.bruzzese@gmail.com>
2024-09-03 18:29:16 -0300

    Terminada la animacion de flipped.

--- src/app/components/areas/areas.component.ts
+++ src/app/components/areas/areas.component.ts
@@ -8,37 +8,42 @@ import { title } from 'node:process';
 })
 export class AreasComponent {
 
+  flippedIndex: number | null = null;
+
   areas = [
     {
       id: 1,
-      title: "Derecho Administrativo 1",
+      title: "Derecho Penal",
       description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. In quos laboriosam ratione animi, hic perferendis ex reprehenderit",
     },
     {
       id: 2,
-      title: "Derecho Administrativo 2",
+      title: "Derecho de Familia",
       description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. In quos laboriosam ratione animi, hic perferendis ex reprehenderit",
     },
     {
       id: 3,
-      title: "Derecho Administrativo 3",
+      title: "Derecho Previsional",
       description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. In quos laboriosam ratione animi, hic perferendis ex reprehenderit",
     },
     {
       id: 4,
-      title: "Derecho Administrativo 4",
+      title: "Amparos de Salud",
       description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. In quos laboriosam ratione animi, hic perferendis ex reprehenderit",
     },
     {
       id: 5,
-      title: "Derecho Administrativo 5",
+      title: "Derecho Laboral",
       description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. In quos laboriosam ratione animi, hic perferendis ex reprehenderit",
     },
     {
       id: 6,
-      title: "Derecho Administrativo 6",
+      title: "Accidentes de Tránsito y Laborales",
       description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. In quos laboriosam ratione animi, hic perferendis ex reprehenderit",
     },
-
   ];
+
+  toggleCard(index: number) {
+    this.flippedIndex = this.flippedIndex === index ? null : index;
+  }
 }
