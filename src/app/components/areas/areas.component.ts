f4d4f0585596f5b56b95674e7e6124369b4ab3e5
Leonel <leonel.bruzzese@gmail.com>
2024-09-03 17:48:21 -0300

    Realice un ciclo for para traer las distintas areas. Se tocaron estilos para el flipped, falta logica.

--- src/app/components/areas/areas.component.ts
+++ src/app/components/areas/areas.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { title } from 'node:process';
 
 @Component({
   selector: 'app-areas',
@@ -7,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class AreasComponent {
 
+  areas = [
+    {
+      id: 1,
+      title: "Derecho Administrativo 1",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. In quos laboriosam ratione animi, hic perferendis ex reprehenderit",
+    },
+    {
+      id: 2,
+      title: "Derecho Administrativo 2",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. In quos laboriosam ratione animi, hic perferendis ex reprehenderit",
+    },
+    {
+      id: 3,
+      title: "Derecho Administrativo 3",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. In quos laboriosam ratione animi, hic perferendis ex reprehenderit",
+    },
+    {
+      id: 4,
+      title: "Derecho Administrativo 4",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. In quos laboriosam ratione animi, hic perferendis ex reprehenderit",
+    },
+    {
+      id: 5,
+      title: "Derecho Administrativo 5",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. In quos laboriosam ratione animi, hic perferendis ex reprehenderit",
+    },
+    {
+      id: 6,
+      title: "Derecho Administrativo 6",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. In quos laboriosam ratione animi, hic perferendis ex reprehenderit",
+    },
+
+  ];
 }
